25a2d49f63abda366ba2b7eb0c502b3e502ff947
ILMARE <827574984@qq.com>
2016-07-23 21:55:21 +0800

    Initial commit

---
+++ cloudStorage/src/main/java/com/itcast/cloudstorage/global/Constants.java
@@ -0,0 +1,11 @@
+package com.itcast.cloudstorage.global;
+
+/**
+ *
+ */
+public class Constants {
+	public static final String CONSUMER_KEY = "2330724462";
+	public static final String CONSUMER_SECRET = "04f81fc56cc936bfc8f0fa1cef285158";
+	public static final String REDIRECT_URL = "http://vauth.appsina.com/callback1.php";
+
+}
